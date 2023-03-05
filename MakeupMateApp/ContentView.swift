b4c2b81e8dba5c1658293c46233fa1e74e0cec95
Shukriii <shukriahmed12@hotmail.co.uk>
2023-03-05 12:29:00 +0000

    Initial Commit

---
+++ MakeupMateApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MakeupMateApp
+//
+//  Created by Shukri  Ahmed on 05/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MakeupMateApp/MakeupMateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeupMateAppApp.swift
+//  MakeupMateApp
+//
+//  Created by Shukri  Ahmed on 05/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MakeupMateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
